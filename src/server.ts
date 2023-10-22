0e1923b1a2de7afee3f8a60d93b4e1d2574bc12e
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-22 15:31:09 -0700

    updating server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 
 app.use("/api", affirmationRouter);
 
+app.get("/", (_req, res) => {
+  res.status(200).send("Hello! server is running").end();
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
